463ee8a2dc131732493e6109f44eeea0f5fb368e
Taekyun Kim <taekyun.contact@gmail.com>
2023-01-27 15:44:33 +0900

    feat : Implement Entity

--- BE/src/main/java/offworkseekers/unnamed/db/entity/Article.java
+++ BE/src/main/java/offworkseekers/unnamed/db/entity/Article.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Article {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "article_id")
     private Long articleId;
 
