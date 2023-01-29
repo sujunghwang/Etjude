9c36c8356f651554f24c95e47575f7285a3e2678
TannyKim <taekyun.contact@gmail.com>
2023-01-29 22:48:51 +0900

    feat : 스토리 배역 설명 추가

--- BE/src/main/java/offworkseekers/unnamed/api/service/StoryService.java
+++ BE/src/main/java/offworkseekers/unnamed/api/service/StoryService.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import offworkseekers.unnamed.api.response.StoryDetailResponse;
 import offworkseekers.unnamed.api.response.StoryListRecommendedByLikeResponse;
+import offworkseekers.unnamed.api.response.StoryRoleResponse;
+import offworkseekers.unnamed.db.entity.Role;
 import offworkseekers.unnamed.db.entity.Story;
+import offworkseekers.unnamed.db.repository.RoleRepository;
 import offworkseekers.unnamed.db.repository.StoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +23,8 @@ import java.util.Optional;
 public class StoryService {
 
     private final StoryRepository storyRepository;
+
+    private final RoleRepository roleRepository;
 
     public List<StoryListRecommendedByLikeResponse> storyListRecommendedByLike() {
         List<StoryListRecommendedByLikeResponse> storyListRecommendedByLike = storyRepository.getStoryListRecommendedByLike();
@@ -34,6 +40,22 @@ public class StoryService {
     public String storyDescResponse(Long storyId) {
         Optional<Story> story = storyRepository.findById(storyId);
         return story.get().getStoryDesc();
+    }
+
+    public List<StoryRoleResponse> storyRoleList(Long storyId) {
+        List<Role> roles = roleRepository.getStoryRoleList(storyId);
+
+        List<StoryRoleResponse> roleList = new ArrayList<>();
+        for (Role role : roles) {
+            roleList.add(StoryRoleResponse.builder()
+                    .roleId(role.getRoleId())
+                    .roleName(role.getRoleName())
+                    .roleDesc(role.getRoleDesc())
+                    .rolePhotoUrl(role.getRolePhotoUrl())
+                    .build());
+        }
+
+        return roleList;
     }
 
 
--- BE/src/main/java/offworkseekers/unnamed/db/repository/RoleRepository.java
+++ BE/src/main/java/offworkseekers/unnamed/db/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package offworkseekers.unnamed.db.repository;
 import offworkseekers.unnamed.db.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository<Role, Long>, RoleRepositorySupport {
 
 }
