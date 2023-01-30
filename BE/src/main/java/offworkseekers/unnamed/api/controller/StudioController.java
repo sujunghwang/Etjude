b65df29abca72ee0badfa7f187267fdccacd907f
TannyKim <taekyun.contact@gmail.com>
2023-01-30 14:11:21 +0900

    MR :  feature-be/studio -> develop-be-temp

--- BE/src/main/java/offworkseekers/unnamed/api/controller/StudioController.java
+++ BE/src/main/java/offworkseekers/unnamed/api/controller/StudioController.java
@@ -2,14 +2,19 @@ package offworkseekers.unnamed.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import offworkseekers.unnamed.api.request.StudioCreateRequest;
 import offworkseekers.unnamed.api.request.StudioIdWithUserIdRequest;
 import offworkseekers.unnamed.api.response.StudioNavBarResponse;
+import offworkseekers.unnamed.api.response.UserSearchResponse;
 import offworkseekers.unnamed.api.service.StudioService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +30,15 @@ public class StudioController {
         return studioService.getStudioNavbar(request.getStudioId(), request.getUserId());
     }
 
+
+    @PutMapping(value = "/api/v1/studio/create")
+    public void createStudio(@RequestBody @Valid StudioCreateRequest studioCreateRequest) {
+        studioService.saveStudio(studioCreateRequest);
+    }
+
+    @PostMapping(value = "/api/v1/studio/search")
+    public List<UserSearchResponse> searchUser(@RequestBody @Valid Map<String, String> param){
+        String keyword = param.get("keyword");
+        return studioService.searchUser(keyword);
+    }
 }
