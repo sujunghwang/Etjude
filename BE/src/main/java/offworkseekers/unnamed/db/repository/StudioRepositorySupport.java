f6ecfd56ebc171561ba57a069ec27c2041225c40
Taekyun Kim <taekyun.contact@gmail.com>
2023-01-30 11:11:45 +0900

    feat : 유저 아이디 필드 자료형 수정

--- BE/src/main/java/offworkseekers/unnamed/db/repository/StudioRepositorySupport.java
+++ BE/src/main/java/offworkseekers/unnamed/db/repository/StudioRepositorySupport.java
@@ -4,5 +4,5 @@ import offworkseekers.unnamed.api.response.StudioNavBarResponse;
 
 public interface StudioRepositorySupport {
 
-    StudioNavBarResponse findStudioNavbar(Long studioId, Long userId);
+    StudioNavBarResponse findStudioNavbar(Long studioId, String userId);
 }
